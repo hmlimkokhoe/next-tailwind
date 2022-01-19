8ba570de6eccd8b173f02b07d76f98f0821a9df1
kok-hoe.lim <kok-hoe.lim@ippen-digital.de>
2022-01-19 12:54:46 +0100

    Add breadcrumb using normal CSS at first

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -2,16 +2,15 @@ import React from 'react'
 
 function Breadcrumb () {
     return (
-        <nav class="flex" aria-label="Breadcrumb">
-            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-                <li class="inline-flex items-center">
-                    <a class="text-gray-700 hover:text-gray-900 inline-flex items-center" href="#">Home</a>
-                </li>
-                <li class="inline-flex items-center">
-                    <a class="text-gray-700 hover:text-gray-900 ml-1 md:ml-2 text-sm font-medium" href="#">Projects</a>
-                </li>
-                <li class="inline-flex items-center" aria-current="page">FlowBite</li>
-            </ol>
+        <nav className="mb-5 before:content" aria-label="Breadcrumb">
+            <ul class="breadcrumb-container">
+                <li id="first-step" class="active"><p>Goals To Define</p></li>
+                <li id="second-step" class=""><p>Metrics</p></li>
+                <li id="third-step" class=""><p>Theory & Intervention</p></li>
+                <li id="third-step" class=""><p>Variants List</p></li>
+                <li id="third-step" class=""><p>Exp Set-Up Checklist</p></li>
+                <li id="third-step" class=""><p>Preview</p></li>
+            </ul>
         </nav>
     )
 }
