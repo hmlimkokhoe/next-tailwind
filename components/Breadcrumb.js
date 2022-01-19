9930eb44720499261e61c69422343b173e5e7e40
kok-hoe.lim <kok-hoe.lim@ippen-digital.de>
2022-01-19 16:55:25 +0100

    Convert CSS to Tailwind

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import BreadcrumbItem from './BreadcrumbItem'
 
 function Breadcrumb () {
     return (
         <nav className="mb-5 before:content" aria-label="Breadcrumb">
-            <ul class="breadcrumb-container">
-                <li id="first-step" class="active"><p>Goals To Define</p></li>
-                <li id="second-step" class=""><p>Metrics</p></li>
-                <li id="third-step" class=""><p>Theory & Intervention</p></li>
-                <li id="third-step" class=""><p>Variants List</p></li>
-                <li id="third-step" class=""><p>Exp Set-Up Checklist</p></li>
-                <li id="third-step" class=""><p>Preview</p></li>
+            <ul className="breadcrumb-container flex w-100 p-0 m-0 first:after:content-[none]">
+                <BreadcrumbItem name={"Goals to define"} isDone={true}/>
+                <BreadcrumbItem name={"Metrics"} isDone={true}/>
+                <BreadcrumbItem name={"Theory & Intervention"} isActive={true}/>
+                <BreadcrumbItem name={"Variants List"}/>
+                <BreadcrumbItem name={"Exp Set-Up Checklist"}/>
+                <BreadcrumbItem name={"Preview"}/>
             </ul>
         </nav>
     )
